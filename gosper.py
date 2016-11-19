bcd334095e53a41e44e35b83bda573c9456039dc
Leo Singer <leo.singer@ligo.org>
2016-11-18 22:13:12 -0500

    Add byline

--- gosper.py
+++ gosper.py
@@ -5,6 +5,8 @@
                ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
               Two procedurally generated fractal beer coasters.
 
+                               by Leo P. Singer
+
                         This work is licensed under a
       Creative Commons Attribution-ShareAlike 4.0 International License
               (http://creativecommons.org/licenses/by-sa/4.0/).
